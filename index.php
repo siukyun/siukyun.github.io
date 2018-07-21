66294853cad3e293e24af6aad37edc0875aa81b1
siukyun <siukyun@yahoo.com>
2018-07-21 16:23:05 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type"content="text/html; charset=UTF-8"/>
+<title>@siukyun | <?php echo date("Y"); ?></title>
+<style type="text/css"media="all">
+html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    background-color: #ebebeb;
+}
+
+.header {
+    height: 50%;
+    margin-bottom: -150px;
+    position: relative;
+}
+
+.content {
+    clear: both;
+    height: 300px;
+    position: relative;
+    margin: 0 auto;
+    width: 50%;
+    text-align: center;
+}
+
+.logo img {
+    height: 300px;
+    border-top-left-radius: 12em;
+    border-top-right-radius: 1em;
+    border-bottom-right-radius: 12em;
+    border-bottom-left-radius: 1em;
+    background-color: white;
+}
+
+.footer {
+    display: none;
+}
+
+</style>
+</head>
+<body>
+<div class="header"></div>
+<div class="content">
+<div class="logo"><img src="/img/logo.jpg"alt="@siukyun"height="300"></div>
+</div>
+</body>
+</html>
